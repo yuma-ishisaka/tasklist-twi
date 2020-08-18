8c14926be21e94a577f2ba32e0eae4fd3573ab39
yuma-ishisaka <iy.opb.smlh@gmail.com>
2020-08-18 08:30:28 +0000

    kadaitw-Ver.3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
   
   def index
