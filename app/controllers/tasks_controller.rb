9f171693bd8f26eca5724769f1a89a019d9b5405
yuma-ishisaka <iy.opb.smlh@gmail.com>
2020-08-18 08:19:00 +0000

    kadaitw-Ver.2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
   
   def index
    if logged_in?
@@ -62,4 +63,12 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content, :status, :user_id)
   end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,11 @@ Rails.application.routes.draw do
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
   root to: 'tasks#index'
   get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  resources :users, only: [:show, :new, :create]
+  resources :users, only: [:new, :create]
   resources :tasks
 end
